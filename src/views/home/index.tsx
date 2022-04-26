deee61fbe42b6cef24c0017bd33ed02fb81657bc
tylerolli <ollityler@gmail.com>
2022-04-26 18:36:22 -0400

    Display $PEW balance on btn click

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,5 +1,5 @@
 // Next, React
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useState, useRef } from 'react';
 import Link from 'next/link';
 
 // Wallet
@@ -13,38 +13,69 @@ import pkg from '../../../package.json';
 
 // Store
 import useUserSOLBalanceStore from '../../stores/useUserSOLBalanceStore';
+import {TOKEN_PROGRAM_ID} from "@solana/spl-token";
 
 export const HomeView: FC = ({ }) => {
   const wallet = useWallet();
   const { connection } = useConnection();
+  const web3 = require("@solana/web3.js");
+  let [count, setCount] = useState(0);
+  const pew = useRef(0);
 
-  const balance = useUserSOLBalanceStore((s) => s.balance)
-  const { getUserSOLBalance } = useUserSOLBalanceStore()
+  const getPewBalance = async (MY_WALLET_ADDRESS) => {
+    const connection = new web3.Connection("https://dawn-black-haze.solana-mainnet.quiknode.pro/2b865c1f0dda4e7e69f261035a6f2bffef825b22/");
+
+    const accounts = await connection.getParsedProgramAccounts(
+        TOKEN_PROGRAM_ID,
+        {
+          filters: [
+            {
+              dataSize: 165, // number of bytes
+            },
+            {
+              memcmp: {
+                offset: 32, // number of bytes
+                bytes: MY_WALLET_ADDRESS, // base58 encoded string
+              },
+            },
+          ],
+        }
+    );
+    const pewToken =  accounts.filter(function(account) {
+      return account.account.data["parsed"]["info"]["mint"] == "AvYRFjk4imoGSAYxcwfm4dM6qQigR6DZJt3vDatdgPeP";
+    });
+    pewToken.forEach((account, i) => {
+      let pewAmount = account.account.data["parsed"]["info"]["tokenAmount"]["uiAmount"]
+      pew.current = pewAmount;
+    });
+  };
 
   useEffect(() => {
     if (wallet.publicKey) {
-      console.log("Wallet Connected Successfully: " + wallet.publicKey.toBase58())
-      getUserSOLBalance(wallet.publicKey, connection)
+      let MY_WALLET_ADDRESS = wallet.publicKey.toBase58();
+      getPewBalance(MY_WALLET_ADDRESS);
     }
-  }, [wallet.publicKey, connection, getUserSOLBalance])
+  }, [wallet.publicKey, connection])
 
   return (
-
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
+        <button onClick={() => setCount(pew.current)}>
+          View PEW Balance
+        </button>
+        <h1>You have {pew.current} $PEW</h1>
         <h1 id="oracle-headline" className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
           Ask The Oracle 
         </h1>
         <h4 className="md:w-full text-center text-slate-300 my-2">
           <p id="oracle-subtext">Peer into the future of SolGats' journey<br></br>
           with the all seeing eye of The Oracle.</p>
-          
+
         </h4>
             
         <div className="text-center oracle-move-cover">
-            <ShowOracle/> 
+          <ShowOracle />
         </div>
-
       </div>
     </div>
   );
